976f8f7a2efdf5e7f905ae307966d0d6d3c265cf
elvis sylejmani <elvissylejmani13@gmail.com>
2019-12-21 00:05:34 +0100

    countdown added

--- ModuliIadministratorit/Core/deletefile.php
+++ ModuliIadministratorit/Core/deletefile.php
@@ -1,8 +1,26 @@
 <?php
 require 'Boot.php';
-echo $id = $_GET['uid'];
+    $id = $_GET['uid'];
 $file = $_GET['name'];
 mysqli_query($conn,"CALL deletefile('$id')");
-if (unlink('../../ModuliIPerdoruesit/uploads/'.$file)) {
+if (file_exists('../../ModuliIPerdoruesit/uploads/'.$file)) {
+    unlink('../../ModuliIPerdoruesit/uploads/'.$file);
     header('Location: ../index.php');
+}else{
+    echo "File nuk u gjet, ju lutem rishikoni fillin.";
+    header('Refresh: 10; URL=../index.php');
 }
+?>
+<p>ju do te ktheheni mbrapa per  <span id="counter">10</span> seconda.</p>
+<script type="text/javascript">
+function countdown() {
+    var i = document.getElementById('counter');
+    if (parseInt(i.innerHTML)<=0) {
+        location.href = 'login.php';
+    }
+if (parseInt(i.innerHTML)!=0) {
+    i.innerHTML = parseInt(i.innerHTML)-1;
+}
+}
+setInterval(function(){ countdown(); },1000);
+</script>
